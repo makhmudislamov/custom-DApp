9cd9f7ab2cf117233a877d5f23d4cd08b96baf53
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-07-04 18:41:09 -0700

    adding tenants, migrated the changes

--- contracts/Rating.sol
+++ contracts/Rating.sol
@@ -1,18 +1,41 @@
 pragma solidity ^0.5.0;
 
 /**
- * @title Rating
+ * @title Rating - rating for tenants by landlords/landladies
+ * @author Makhmud Islamov
+ * @notice You can use this contract for only the most basic simulation
  * Creature - a contract for rating tenants
  */
 
  contract Rating {
-     /// state variable tenant
-     string public tenant;
+
+     /// Model: Tenant
+     struct Tenant {
+         uint id;
+         string name;
+         uint rateCount;
+     }
+
+     /// Fetching tenants: persisting tenants into storage
+     mapping(uint => Tenant) public tenants;
+
+     /// storing tenant count
+     uint public tenantsCount;
      
      /// constructor
      constructor () public {
-         tenant = "Max Payne";
+         /// adding tenants
+         addTenant("Max Payne");
+         addTenant("John Wick");
+         addTenant("Jack Reacher");
+         addTenant("Lara Croft");
 
      }
+
+    /// adding new Tenants to the app
+    function addTenant (string memory _name) private {
+        tenantsCount ++;
+        tenants[tenantsCount] = Tenant(tenantsCount, _name, 0);
+    }
 
  }
